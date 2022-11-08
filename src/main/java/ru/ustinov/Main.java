c8bd5d6668006e1162f88daf683eba28053af1eb
Ustinov George <ustinovgo@gmail.com>
2022-11-08 15:08:20 +0200

    HW1 Completion

--- src/main/java/ru/ustinov/Main.java
+++ src/main/java/ru/ustinov/Main.java
@@ -2,7 +2,32 @@ package ru.ustinov;
 
 
 public class Main {
+    private static final int LEFT_BORDER = 0;
+    private static final int RIGHT_BORDER = 92;
+    private static final int START_POSITION = 2;
+
+    private static void fibonacci(int position) {
+        long previous = 0;
+        long current = 1;
+        if (position > RIGHT_BORDER || position < LEFT_BORDER) {
+            System.out.println("Incorrect value!");
+        } else if (position == LEFT_BORDER) {
+            System.out.println(previous);
+        }
+        for (int i = START_POSITION; i <= position; ++i) {
+            long next = current + previous;
+            previous = current;
+            current = next;
+        }
+        if (position >= LEFT_BORDER && position <= RIGHT_BORDER) {
+            System.out.println(current);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        fibonacci(92);
+        fibonacci(10);
+        fibonacci(-1);
+        fibonacci(228);
     }
 }
